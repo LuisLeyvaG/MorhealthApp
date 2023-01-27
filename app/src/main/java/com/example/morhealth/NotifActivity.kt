14ec216c1b103069e73d7cb495f3a25a73cdeba5
LuisG <leyva.garcia.luis.gerardo@gmail.com>
2023-01-27 13:33:52 -0600

    Menu desplegable mejorado

---
+++ app/src/main/java/com/example/morhealth/NotifActivity.kt
@@ -0,0 +1,12 @@
+package com.example.morhealth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.morhealth.R
+
+class NotifActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notif)
+    }
+}
