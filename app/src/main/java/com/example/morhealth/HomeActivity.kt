99b1f1e2688c4b9d0f3f83758cbcc97bf88a8cb7
LuisG <leyva.garcia.luis.gerardo@gmail.com>
2023-01-27 02:25:38 -0600

    Se ha creado el menu desplegable de el inicio

--- app/src/main/java/com/example/morhealth/HomeActivity.kt
+++ app/src/main/java/com/example/morhealth/HomeActivity.kt
@@ -2,19 +2,56 @@ package com.example.morhealth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.morhealth.databinding.ActivityHomeBinding
 import com.example.morhealth.databinding.ActivityLoginBinding
+import com.google.android.material.navigation.NavigationView
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var binding: ActivityHomeBinding
+
+    private lateinit var drawer: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initToolbar()
+        initNavigationView()
 
+    }
 
+    private fun initToolbar() {
+        val toolbar: Toolbar = findViewById(R.id.toolbar_main)
+        setSupportActionBar(toolbar)
+
+        drawer = findViewById(R.id.drawer_layout)
+        val toggle = ActionBarDrawerToggle(this, drawer, toolbar, R.string.bar_title, R.string.navigation_drawer_close)
+
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+
+    }
+
+    private fun initNavigationView() {
+        var navigationView: NavigationView = findViewById(R.id.nav_view)
+
+        navigationView.setNavigationItemSelectedListener(this)
+
+        var headerView: View = LayoutInflater.from(this).inflate(R.layout.nav_header_main, navigationView, false)
+        navigationView.removeHeaderView(headerView)
+        navigationView.addHeaderView(headerView)
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        TODO("Not yet implemented")
     }
 }
